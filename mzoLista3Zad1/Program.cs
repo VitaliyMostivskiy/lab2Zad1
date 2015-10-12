9034e96204cfc83edf9d618a3c41b96fc0af07e3
unknown <vitaliymostivskiy@gmail.com>
2015-10-12 10:10:16 +0200

    Initial commit

---
+++ mzoLista3Zad1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace mzoLista3Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = GetStudents("zad1.csv");
+
+            foreach (var student in students)
+            {
+                student.Print();
+            }
+        }
+
+        private static List<Student> GetStudents(string path)
+        {
+            List<Student> students = new List<Student>();
+
+            using (var file = new StreamReader(path))
+            {
+                string line = "";
+                while ((line = file.ReadLine()) != null)
+                {
+                    var parts = line.Split("><".ToCharArray());
+                    students.Add(new Student { Name = parts[1], Surname = parts[3] });
+                }
+            }
+
+            return students;
+        }
+    }
+}
---
+++ mzoLista3Zad1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace mzoLista3Zad1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Name: {0}\nSurname: {1}", Name, Surname);
+        }
+    }
+}
+
